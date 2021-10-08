d2084f3d0f858272a5686cb4e415716980c5d89e
moon18004 <purnsup90@gmail.com>
2021-10-07 23:08:49 -0600

    JS home contact

--- main.js
+++ main.js
@@ -21,6 +21,17 @@ navbarMenu.addEventListener('click', (event)=>{
         return;
     }
     console.log(link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+    scrollIntoView(link);
 })
+
+// Handle click on "contact me" button on home
+const contact = document.querySelector('.home__contact');
+contact.addEventListener('click', () => {
+    scrollIntoView('#contact')
+    
+} )
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+}
