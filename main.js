1c275b7dec8e64a61e3a7e7740c4b5e492defdeb
moon18004 <purnsup90@gmail.com>
2021-10-09 16:24:44 -0600

    js arrow up

--- main.js
+++ main.js
@@ -45,3 +45,18 @@ function scrollIntoView(selector) {
     scrollTo.scrollIntoView({behavior: 'smooth'});
 }
 
+// show "arrow up" button when scrooling down
+const arrowUp = document.querySelector('.arrow-up')
+document.addEventListener('scroll', ()=>{
+    if(window.scrollY > homeHeight /2){
+        arrowUp.classList.add('visible');
+    }
+    else{
+        arrowUp.classList.remove('visible');
+    }
+})
+
+//Handle clink on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('#home');
+})
